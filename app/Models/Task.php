a80dd229727e42eb6c2a44278d3e391a9864a1bb
kujawapatryk <homik363@wp.pl>
2023-10-11 01:20:27 +0200

    Hiding 'created_at' and 'updated_at' data during conversion.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,8 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static find(mixed $id)
+ */
 class Task extends Model
+
 {
+    protected $hidden = ['created_at', 'updated_at'];
     protected $fillable = [
         'content',
         'done',
